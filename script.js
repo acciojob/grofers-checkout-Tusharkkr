abe3cab40b36943bee45c48322dbf3a76d3457a4
Tusharkkr <narutoboi41@gmail.com>
2026-01-30 22:12:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,22 @@
 const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
+        let td = document.querySelectorAll('.price')
+        let table = document.querySelector('table')
+        let tablerow = document.createElement('tr')
+        let tabledata = document.createElement('td')
 
 const getSum = () => {
-    let prices = document.querySelectorAll('.price');
-    let sum = 0;
-
-    for (let t of prices) {
-        sum += Number(t.innerText);
-    }
-
-    const table = document.querySelector("table");
-
-    // remove old total row if exists
-    let oldRow = document.getElementById("total");
-    if (oldRow) oldRow.remove();
-
-    // create new row
-    let tr = document.createElement("tr");
-    tr.id = "total";
-
-    let td = document.createElement("td");
-    td.colSpan = 2;
-    td.innerText = `Total Price : Rs ${sum}`;
-    td.style.fontWeight = "bold";
-    td.style.textAlign = "center";
-
-    tr.appendChild(td);
-    table.appendChild(tr);
+//Add your code here
+	let sum=0
+            for(let t of td){
+                sum+=+t.innerText
+            }
+			tabledata.innerText=`Total price : ${sum}`
+            tablerow.append(tabledata)
+            table.append(tablerow)
+  
 };
 
 getSumBtn.addEventListener("click", getSum);
+
